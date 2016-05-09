e7532564d302c8dc75db943e57a4f945ffe865a0
Pablo Bussi <pbussi@gmail.com>
2016-05-09 09:00:16 -0300

    FOS

--- entreparticulares/src/EP/EPBundle/Entity/Usuario.php
+++ entreparticulares/src/EP/EPBundle/Entity/Usuario.php
@@ -1,159 +1,25 @@
 <?php
-
 namespace EP\EPBundle\Entity;
-
 use Doctrine\ORM\Mapping as ORM;
 
+use FOS\UserBundle\Model\User as BaseUser;
+
 /**
- * Usuario
- *
  * @ORM\Table(name="usuario")
  * @ORM\Entity
  */
-class Usuario
-{
+class Usuario extends BaseUser {
+
     /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="username", type="string", length=45, nullable=false)
-     */
-    private $username;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="apellido", type="string", length=255, nullable=false)
-     */
-    private $apellido;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="nombre", type="string", length=255, nullable=false)
-     */
-    private $nombre;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="password", type="string", length=45, nullable=false)
-     */
-    private $password;
-
-
-
-    /**
-     * Set username
-     *
-     * @param string $username
-     *
-     * @return Usuario
-     */
-    public function setUsername($username)
-    {
-        $this->username = $username;
-
-        return $this;
+    public function __construct() {
+        parent::__construct();
+        //do domething
     }
 
-    /**
-     * Get username
-     *
-     * @return string
-     */
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
-    /**
-     * Set apellido
-     *
-     * @param string $apellido
-     *
-     * @return Usuario
-     */
-    public function setApellido($apellido)
-    {
-        $this->apellido = $apellido;
-
-        return $this;
-    }
-
-    /**
-     * Get apellido
-     *
-     * @return string
-     */
-    public function getApellido()
-    {
-        return $this->apellido;
-    }
-
-    /**
-     * Set nombre
-     *
-     * @param string $nombre
-     *
-     * @return Usuario
-     */
-    public function setNombre($nombre)
-    {
-        $this->nombre = $nombre;
-
-        return $this;
-    }
-
-    /**
-     * Get nombre
-     *
-     * @return string
-     */
-    public function getNombre()
-    {
-        return $this->nombre;
-    }
-
-    /**
-     * Set password
-     *
-     * @param string $password
-     *
-     * @return Usuario
-     */
-    public function setPassword($password)
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * Get password
-     *
-     * @return string
-     */
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
 }
